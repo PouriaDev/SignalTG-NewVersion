f14bebbf5955339f3ea5080f938da36b71d3db95
p00ria <amir1300sobatdar@gmail.com>
2016-12-30 21:20:26 +0330

    Update GroupManager.lua

--- plugins/GroupManager.lua
+++ plugins/GroupManager.lua
@@ -1,17 +1,26 @@
 local function run(msg, matches)
 if matches[1] == 'setname' and is_momod(msg) then
         tg.changeChatTitle(chat_id, string.sub(matches[2], 10), 1)
-        tg.sendMessage(msg.chat_id, msg.id_, 1,'<b>SuperGroup Name Changed To </b><code>'..string.sub(matches[2], 10)..'</code>', 1, 'html')
+        tg.sendMessage(msg.chat_id, 0, 1,'<b>SuperGroup Name Changed To </b><code>'..string.sub(matches[2], 10)..'</code>', 1, 'html')
 end
 if matches[1] == 'edit' and is_momod(msg) then
-        tg.editMessageText(msg.chat_id, reply_id, nil, string.sub(matches[2], 7), 'html')
+        tg.editMessageText(msg.chat_id, msg.reply_to_message_id_, nil, string.sub(matches[2], 7), 'html')
+end 
+if matches[1] == 'pin' and is_owner(msg) then
+        tg.pinChannelMessage(msg.chat_id_, msg.reply_to_message_id_, 1)
+        tg.sendMessage(msg.chat_id_, 0, 0,  '<b>Message Has Been Pinned!</b>', 0, 'html')
+end
+if matches[1] == 'unpin' and is_owner(msg) then
+        tg.unpinChannelMessage(msg.chat_id_)
+        tg.sendMessage(msg.chat_id_, 0, 0,  '<b>Message Has Been Unpinned!</b>', 0, 'html')
+end
 end
 return {
   patterns = {
     "^[#!/](setname) (.*)$",
     "^[#!/](edit) (.*)$",
-    "^[#!/](changename) (.*)$",
-    "^[#!/](changeusername) (.*)$",
+    "^[#!/](pin)$",
+    "^[#!/](unpin)$",
   },
   run = run,
 }
