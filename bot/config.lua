bbc4e518ee16bb0a4b7bbcdf31442bfcdaf2f9b1
p00ria <amir1300sobatdar@gmail.com>
2016-12-30 21:29:29 +0330

    Update config.lua

--- bot/config.lua
+++ bot/config.lua
@@ -1,18 +1,17 @@
 do local _ = {
   enabled_plugins = {
     "addrem",
-    "bc",
+    "Plugins",
     "lock",
     "msg_checks",
-    "pin"
+    "Admin",
+    "GroupManager"
   },
   group  = {
     data = "bot/group.json"
   },
   sudo_users = {
-    268021336,
-    260446917,
-    223404066
+    142141024,
   },
 robot = {
     306386341,
